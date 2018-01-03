8865d125d7111edd4a4148a6daa5f8ff0afc2f06
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:58 -0300

    2016.12.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,24 @@
 (ns advent.core)
+
+(defn run [instructions]
+  (let [reg-or-val (fn [reg x] (or (reg (keyword x)) (Integer. x)))]
+    (loop [reg {:a 0 :b 0 :c 0 :d 0}
+           i 0]
+      (if-let [instruction (get instructions i)]
+        (let [[inst x y] (rest (re-find #"(\w+) ([\w-]+)(?: ([\w-]+))?" instruction))]
+          (case inst
+            "cpy" (recur (assoc reg (keyword y) (reg-or-val reg x))
+                         (inc i))
+            "inc" (recur (update reg (keyword x) inc)
+                         (inc i))
+            "dec" (recur (update reg (keyword x) dec)
+                         (inc i))
+            "jnz" (recur reg
+                         (+ i (if (zero? (reg-or-val reg x)) 1 (Integer. y))))))
+        reg))))
+
+(defn solve [instructions]
+  (:a (run (clojure.string/split-lines instructions))))
+
+(defn -main []
+  (println (solve (slurp "resources/input.txt"))))
