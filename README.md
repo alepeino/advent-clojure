f577e9450ed78a1f08b13a83fc9ad63b00f95db1
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:58 -0300

    2016.1.2

--- README.md
+++ README.md
@@ -20,3 +20,13 @@ Following **R2**, **L3** leaves you 2 blocks East and 3 blocks North, or 5 block
 **R5**, **L5**, **R5**, **R3** leaves you 12 blocks away.
 How many blocks away is Easter Bunny HQ?
 
+---
+
+### Part Two
+
+Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+
+For example, if your instructions are **R8**, **R4**, **R4**, **R8**, the first location you visit twice is 4 blocks away, due East.
+
+How many blocks away is the first location you visit twice?
+
--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,8 +1,8 @@
 (ns advent.core)
 
-(defn distance-to-center [{[x y] :pos}]
-  (+ (Math/abs x)
-     (Math/abs y)))
+(defn turn [data turn-dir]
+  (let [turn-fn ({\L dec \R inc} turn-dir)]
+    (update data :dir #(-> % turn-fn (mod 4)))))
 
 (defn step-increment [dir]
   (case dir
@@ -11,27 +11,38 @@
     2 [0 -1]
     3 [-1 0]))
 
-(defn new-pos [current dir steps]
-  (map +
-    current
-    (map * [steps steps] (step-increment dir))))
+(defn walk [data steps]
+  (let [increment (step-increment (:dir data))]
+    (reduce
+      (fn [{:keys [visited] :as data-r}  _]
+        (let [new-loc (map + (last visited) increment)
+              visited (conj visited new-loc)
+              new-data (assoc data-r :visited visited)]
+          (if (apply distinct? visited)
+            new-data
+            (reduced (assoc new-data :end true)))))
+      data
+      (range steps))))
 
-(defn new-dir [current turn]
-  (mod (({\L dec \R inc} turn) current)
-       4))
-
-(defn move [{:keys [dir pos]} [turn & steps]]
+(defn move [data [turn-dir & steps]]
   (let [steps (Integer. (apply str steps))
-        new-dir (new-dir dir turn)
-        new-pos (new-pos pos new-dir steps)]
-    {:dir new-dir
-     :pos new-pos}))
+        turned (turn data turn-dir)]
+    (walk turned steps)))
+
+(defn distance-to-center [data]
+  (let [[x y] (last (:visited data))]
+    (+ (Math/abs x)
+       (Math/abs y))))
 
 (defn solve [input]
   (distance-to-center
     (reduce
-      move
-      {:dir 0 :pos [0 0]}
+      (fn [data movement]
+        (let [new-data (move data movement)]
+          (if (:end new-data)
+            (reduced new-data)
+            new-data)))
+      {:dir 0 :visited [[0 0]]}
       input)))
 
 (defn -main []
