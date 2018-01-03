d4045a0a77824b8a16ac3f9cc854d31bddc19c18
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:58 -0300

    2016.15.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,24 @@
 (ns advent.core)
+
+(defn parse-input [input]
+  (->> (clojure.string/split-lines input)
+    (map (partial re-find #".+?(\d+) positions.+at position (\d+)."))
+    (map (fn [[_ positions start]] {:positions (Integer. positions)
+                                    :start (Integer. start)}))))
+
+(defn solve [input]
+  (let [discs (parse-input input)
+        endpos (map-indexed (fn [index {:keys [positions]}]
+                              (->> (dec (- index))
+                                (iterate (partial + positions))
+                                (drop-while neg?)
+                                (first)))
+                            discs)]
+    (->> discs
+      (map #(->> (range (:positions %)) (cycle) (drop (:start %))))
+      (apply map vector)
+      (keep-indexed #(when (#{endpos} %2) %1))
+      (first))))
+
+(defn -main []
+  (println (solve (slurp "resources/input.txt"))))
