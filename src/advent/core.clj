ecbfde34331c14e14bec3ca0a8755fbde34dd2b0
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:58 -0300

    2016.13.2

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -17,7 +17,7 @@
     (pos? y) (conj [x (dec y)])
     (pos? x) (conj [(dec x) y])))
 
-(defn shortest [start finish input]
+(defn locations [start steps input]
   (let [open? (partial open? input)]
     (loop [distances {start 0}
            visited #{}]
@@ -29,11 +29,12 @@
                                   (update m node #(if % (min % d) d))))
                                distances
                                (filter open? (adjacents current)))]
-        (or (distances' finish)
-            (recur distances' (conj visited current)))))))
+        (if (> (distances' current) steps)
+          (count (filter (comp (partial >= steps) second) distances'))
+          (recur distances' (conj visited current)))))))
 
 (defn solve [input]
-  (shortest [1 1] [31 39] input))
+  (locations [1 1] 50 input))
 
 (defn -main []
   (println (solve (slurp "resources/input.txt"))))
