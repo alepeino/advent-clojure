96c7d14e6c307d41a109f80d00eb03b6023397fb
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:59 -0300

    2016.18.1

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject advent "0.1.0-SNAPSHOT"
   :description ""
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"]]
   :main advent.core)
--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,25 @@
-(ns advent.core)
+(ns advent.core
+   (:require [clojure.core.match :refer [match]]))
+
+(defn get-tile [previous-row]
+  (match (vec previous-row)
+    [\^ \^ \.] \^
+    [\. \^ \^] \^
+    [\^ \. \.] \^
+    [\. \. \^] \^
+    :else \.))
+
+(defn next-row [row]
+  (->> (concat [\.] row [\.])
+    (partition 3 1)
+    (map get-tile)))
+
+(defn solve [input]
+  (->> input
+    (iterate next-row)
+    (take 40)
+    (mapcat (partial keep #{\.}))
+    (count)))
+
+(defn -main []
+  (println (solve (slurp "resources/input.txt"))))
