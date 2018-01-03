e77d4f4fd4e0717ebf08dd17e238e4652b6f2a1b
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:59 -0300

    2016.5.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,26 @@
 (ns advent.core)
+
+(defn md5
+  "MD5 hash (https://gist.github.com/jizhang/4325757)"
+  [s]
+  (let [algorithm (java.security.MessageDigest/getInstance "MD5")
+        raw (.digest algorithm (.getBytes s))]
+    (format "%032x" (java.math.BigInteger. 1 raw))))
+
+(defn find-password [id]
+  (->> (range)
+    (map (partial str id))
+    (map md5)
+    (filter #(clojure.string/starts-with? % "00000"))
+    (take 8)
+    (map #(nth % 5))
+    (apply str)))
+
+(defn input [content]
+  content)
+
+(defn solve [input]
+  (find-password input))
+
+(defn -main []
+  (prn (solve (input (slurp "resources/input.txt")))))
