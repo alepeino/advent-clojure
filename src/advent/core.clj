cb3e93a4e3fff58a5bb5c5786b4189a375a2a52a
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:59 -0300

    2016.7.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,32 @@
 (ns advent.core)
+
+(defn input [strs]
+  (->> strs
+    (clojure.string/split-lines)
+    (map clojure.string/trim)))
+
+(defn has-abba? [s]
+  (let [[a b b2 a2 & more] s]
+    (or (and (not= a b)
+             (= a a2)
+             (= b b2))
+        (when (seq more)
+	  (has-abba? (next s))))))
+
+(defn supports-tls? [s]
+  (->> s
+    (partition-by #{\[ \]})
+    (take-nth 2)
+    ((juxt (partial take-nth 2)
+           (comp (partial take-nth 2) rest)))
+    ((fn [[out in]]
+      (and (some has-abba? out)
+           (not-any? has-abba? in))))))
+
+(defn solve [input]
+  (->> input
+    (filter supports-tls?)
+    (count)))
+
+(defn -main []
+  (prn (solve (input (slurp "resources/input.txt")))))
