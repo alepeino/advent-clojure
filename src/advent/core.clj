3da01178780df4792b1c3fd47b1c1a096b3106a8
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:59 -0300

    2016.6.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,17 @@
 (ns advent.core)
+
+(defn input [strs]
+  (->> strs
+    (clojure.string/split-lines)
+    (map clojure.string/trim)))
+
+(defn solve [input]
+  (->> input
+    (apply map str)
+    (map frequencies)
+    (map (partial sort-by second))
+    (map (comp first last))
+    (apply str)))
+
+(defn -main []
+  (prn (solve (input (slurp "resources/input.txt")))))
