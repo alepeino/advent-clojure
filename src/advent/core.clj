e2048cce37fc38cc5a6fbdc8e38c7d4cf01e6775
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:58 -0300

    2016.14.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,23 @@
 (ns advent.core)
+
+(defn md5 [s]
+  (->> (.getBytes s)
+    (.digest (java.security.MessageDigest/getInstance "MD5"))
+    (java.math.BigInteger. 1)
+    (format "%032x")))
+
+(defn solve [salt]
+  (let [hasher (memoize md5)
+        hashes (map (comp hasher (partial str salt)) (range))]
+    (->> (range)
+      (filter (fn [n]
+                (when-let [[_ c] (re-find #".*?(.)\1\1.*" (nth hashes n))]
+                  (let [re (re-pattern (apply str (repeat 5 c)))]
+                    (when (some (partial re-find re)
+                                (->> hashes (drop (inc n)) (take 1000)))
+                      n)))))
+      (take 64)
+      (last))))
+
+(defn -main []
+  (println (solve (slurp "resources/input.txt"))))
