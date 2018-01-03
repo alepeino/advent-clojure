2c4d12ca1e2721d60ed06b299d6ed0e3b03fa73b
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:59 -0300

    2016.19.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,16 @@
 (ns advent.core)
+
+(defn pass [elves]
+  (if (even? (count elves))
+    (take-nth 2 elves)
+    (cons (last elves) (pass (butlast elves)))))
+
+(defn solve [input]
+  (->> (range 1 (inc (Integer. input)))
+    (iterate pass)
+    (drop-while #(> (count %) 1))
+    (first)
+    (first)))
+
+(defn -main []
+  (println (solve (slurp "resources/input.txt"))))
