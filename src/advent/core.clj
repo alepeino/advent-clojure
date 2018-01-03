4e017a53ea55c726e198b60cf09675929daab0a5
Alejandro Peinó <alepeino@gmail.com>
2018-01-03 12:23:59 -0300

    2016.3.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,1 +1,20 @@
 (ns advent.core)
+
+(defn valid? [sides]
+  (let [perimeter (apply + sides)]
+    (every? #(< % (/ perimeter 2)) sides)))
+
+(def input
+  (->> "resources/input.txt"
+    (slurp)
+    (clojure.string/split-lines)
+    (map #(as-> % $
+           (clojure.string/trim $)
+           (clojure.string/split $ #"\s+")
+           (map (fn [s] (Integer. s)) $)))))
+
+(defn solve [input]
+  (count (filter valid? input)))
+
+(defn -main []
+  (prn (solve input)))
---
+++ test/advent/core_test.clj
@@ -0,0 +1,4 @@
+(ns advent.core-test
+  (:require [clojure.test :refer :all]
+            [advent.core :refer :all]))
+
