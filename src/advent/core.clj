146c4a7f1518fc27213810c31108ff675f1f70b4
Alejandro Peinó <alepeino@gmail.com>
2018-01-08 16:16:54 -0300

    2016.20.1

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,7 +1,18 @@
 (ns advent.core)
 
+(defn parse-ranges [input]
+  (->> input
+    (clojure.string/split-lines)
+    (map #(clojure.string/split % #"-"))
+    (map #(map (fn [n] (BigInteger. n)) %))
+    (sort-by first)))
+
 (defn solve [input]
-  input)
+  (loop [[[range-lo range-hi] & more] (parse-ranges input)
+         current-ip (BigInteger. "0")]
+    (if (>= current-ip range-lo)
+      (recur more (inc range-hi))
+      current-ip)))
 
 (defn -main []
   (println (solve (slurp "resources/input.txt"))))
